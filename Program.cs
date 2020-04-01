aa63773e298ec140920fa26b2ebf82a0fa7d19b3
unknown <aminmirzoev123@gmail.com>
2020-04-02 00:18:37 +0500

    HW-6

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace Новая_папка
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        //      Console.Write("Введите значение массива:");
+        //     int array = int.Parse(Console.ReadLine());
+        //     int[] N = new int[array];
+        //     int f=0,Max=N[0],Min=N[0],W=0;
+        //     System.Console.WriteLine("Общая сумма всех элементов");
+        //     for (int i = 0; i < array; i++)
+        //     {
+        //         N[i]=i+1;
+        //         f+=N[i];
+        //         Console.WriteLine(f);
+        //             W=f/N[i];
+        //     }
+        //         System.Console.WriteLine("Значение Max and Min");
+        //     for (int i = 0; i < array; i++)
+        //     {
+        //         if(Max<N[i])
+        //         {
+        //             Max = N[i];
+        //         }
+        //         if(Min>N[i]){
+        //             Min = N[i];
+        //         }
+               
+        //     }
+        //     Console.WriteLine(Max);
+        //     Console.WriteLine(Min);
+
+        //             System.Console.WriteLine("нечетные значения");
+        //      for (int i = 0; i < array; i++)
+        //     {
+        //        if(N[i]%2!=0)
+        //        {
+        //            System.Console.WriteLine(N[i]);
+        //        }
+        //     }
+        //         System.Console.WriteLine("Среднее арифметическое");
+        //    System.Console.WriteLine(W);
+
+            ///////////////////////////////////////////////////////////////////////////
+           
+                    // int array=int.Parse(Console.ReadLine());
+                    // int[] N=new int[array];
+                    // for (int i = array-1; i <= array; i--)
+                    // {
+                    //     N[i]=i+1;
+                    //     System.Console.WriteLine(N[i]);
+                    // }
+                ////////////////////////////////////////////////////////////////////////
+                
+                
+          Console.ReadKey();
+        }
+
+
+    }
+}
+
